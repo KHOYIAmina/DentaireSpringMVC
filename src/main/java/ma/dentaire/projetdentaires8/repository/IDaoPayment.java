d12e1bd1bf84127b448be26cca5ba3ee0e0ce629
Amina <khoyii.amina@gmail.com>
2024-06-01 14:54:10 +0100

    drop payement commit

--- src/main/java/ma/dentaire/projetdentaires8/repository/IDaoPayment.java
+++
@@ -1,6 +0,0 @@
-package ma.dentaire.projetdentaires8.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IDaoPayment extends JpaRepository<Payment, Integer> {
-}
